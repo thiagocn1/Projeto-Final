0e8687c6f6d5fd9945cf405b2b91594bef5493de
Thiago Naves <thiagocn1@al.insper.edu.br>
2022-06-08 11:06:53 -0300

    recomeçar/sair

--- jogar_novamente.py
+++ jogar_novamente.py
@@ -15,7 +15,7 @@ pygame.mixer.init()
 def jogar_novamente_screen(screen,condicao,WIDTH, HEIGHT):
     #unidade de tempo
     clock=pygame.time.Clock()
-
+    saida = ENCERRAR
 
     #carrega botões
     sair_img = pygame.image.load(path.join(IMG_DIR,'Sair.png')).convert_alpha()
@@ -46,20 +46,35 @@ def jogar_novamente_screen(screen,condicao,WIDTH, HEIGHT):
         #carregar o fundo da tela de menu
         if condicao==VITORIA:
             
-            Derrota=pygame.image.load(path.join(IMG_DIR, 'Vitoria.png')).convert()
-            Derrota_small = pygame.transform.scale(Derrota, (WIDTH, HEIGHT))
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    rodando=False
+            Vitoria=pygame.image.load(path.join(IMG_DIR, 'Vitoria.png')).convert()
+            Vitoria_small = pygame.transform.scale(Vitoria, (WIDTH, HEIGHT))
 
             #primeira tela final
             screen.fill((0,0,0))
             screen.blit(Derrota_small,(0,0))
             pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    rodando=False
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_s:
+                        rodando= False
+                    if event.key == pygame.K_RETURN:
+                        saida =  MENU
+                        rodando = False
         
         elif condicao==EMPATE:
             Derrota=pygame.image.load(path.join(IMG_DIR, 'GameOver.png')).convert()
             Derrota_small = pygame.transform.scale(Derrota, (WIDTH, HEIGHT))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    rodando=False
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_s:
+                        rodando= False
+                    if event.key == pygame.K_RETURN:
+                        saida =  MENU
+                        rodando = False
 
             #segunda tela final
             screen.fill((0,0,0))
@@ -69,11 +84,20 @@ def jogar_novamente_screen(screen,condicao,WIDTH, HEIGHT):
         else:
             Derrota=pygame.image.load(path.join(IMG_DIR, 'GameOver.png')).convert()
             Derrota_small = pygame.transform.scale(Derrota, (WIDTH, HEIGHT))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    rodando=False
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_s:
+                        rodando= False
+                    if event.key == pygame.K_RETURN:
+                        saida =  MENU
+                        rodando = False
 
             #terceira tela de final
             screen.fill((0,0,0))
             screen.blit(Derrota_small,(0,0))
             pygame.display.update()
-    return ENCERRAR
+    return saida
 
 
